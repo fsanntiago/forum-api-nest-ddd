9cda734e3f682272d9339a03c0b762d9ddc8c2dd
fsanntiago <zerofabricio@hotmail.com>
2024-07-17 09:40:06 -0300

    feat: add authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,59 @@
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { Env } from 'src/env'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as crypto from 'crypto'
+import { JwtStrategy } from './jwt.strategy'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKeyBase64 = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKeyBase64 = config.get('JWT_PUBLIC_KEY', { infer: true })
+        const privateKeyPassword = config.get<string>(
+          'JWT_PRIVATE_KEY_PASSWORD',
+          { infer: true },
+        )
+
+        const privateKey = Buffer.from(privateKeyBase64, 'base64').toString(
+          'utf8',
+        )
+        const publicKey = Buffer.from(publicKeyBase64, 'base64').toString(
+          'utf8',
+        )
+
+        // Create a temporary file to store the private key
+        const privateKeyPath = path.join(__dirname, 'private_key.pem')
+        fs.writeFileSync(privateKeyPath, privateKey)
+
+        // Decrypt the private key
+        const decryptedPrivateKey = crypto
+          .createPrivateKey({
+            key: fs.readFileSync(privateKeyPath),
+            format: 'pem',
+            passphrase: privateKeyPassword,
+          })
+          .export({ format: 'pem', type: 'pkcs1' })
+          .toString('utf8')
+
+        // Clean up the temporary file
+        fs.unlinkSync(privateKeyPath)
+
+        return {
+          signOptions: { algorithm: 'RS256' },
+          privateKey: decryptedPrivateKey,
+          publicKey,
+        }
+      },
+    }),
+  ],
+  providers: [JwtStrategy],
+})
+export class AuthModule {}
