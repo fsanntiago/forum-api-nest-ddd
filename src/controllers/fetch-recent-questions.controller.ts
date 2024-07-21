09f8cd4450900129a15773033f6db5426b97053a
fsanntiago <zerofabricio@hotmail.com>
2024-07-21 12:07:50 -0300

    feat: allow fetch recent questions

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { PrismaService } from './../prisma/prisma.service'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { z } from 'zod'
+
+const pageQuerySchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQuerySchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQuerySchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return { questions }
+  }
+}
