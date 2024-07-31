eb87faa219e36c68c1636495c7a4b89a3d721ed3
fsanntiago <zerofabricio@hotmail.com>
2024-07-31 09:17:05 -0300

    feat: add BcryptHasher and JwtEncrypter to cryptography module

---
+++ src/infra/cryptography/cryptography.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common'
+
+import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+
+import { JwtEncrypter } from './jwt-encrypter'
+import { BcryptHasher } from './bcrypt-hasher'
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [
+    {
+      provide: Encrypter,
+      useClass: JwtEncrypter,
+    },
+    {
+      provide: HashGenerator,
+      useClass: BcryptHasher,
+    },
+    {
+      provide: HashComparer,
+      useClass: BcryptHasher,
+    },
+  ],
+  exports: [Encrypter, HashGenerator, HashComparer],
+})
+export class CryptographyModule {}
